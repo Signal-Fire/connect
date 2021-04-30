00e2868f02b218c3fc2ffeb8221c69aea7ef1d68
Michiel van der Velde <michiel@michielvdvelde.nl>
2021-04-30 16:57:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Signal Fire Connect
+
+Connect is a slim client for [Signal Fire Server](https://github.com/signal-fire/server).
+
+> Connect is a **work-in-progress** and as such is not yet published to npm.
+> If you wish to take a crack at it, clone the respository.
+
+> **Note**: Connect uses version 3 of the Signal Fire protocol and is not
+> directly compatible with Signal Fire Server as of this time!
+
+## Example
+
+This example uses [Signal Fire Peer](https://github.com/signal-fire/peer).
+
+```typescript
+import Connect, { IncomingOfferEvent, IncomingAnswerEvent, IncomingICECanidateEvent } from './index'
+import Peer, { OfferEvent, AnswerEvent, ICECandidateEvent } from '@signal-fire/peer'
+
+const connect = new Connect()
+
+// Connect to the signaling server
+await connect.connect('wss://rtc.example.com')
+
+// Create a new peer
+const target = '<target id>'
+const connection = new RTCPeerConnection()
+const peer = new Peer(connection)
+
+peer.addEventListener('offer', ({ detail: offer }: OfferEvent) => {
+  // send the offer to the remote peer through the signaling server
+  connect.sendOffer(target, offer).catch((err: Error) => { /* ... */ })
+})
+
+peer.addEventListener('answer', ({ detail: answer }: AnswerEvent) => {
+  // send the answer to the remote peer through the signaling server
+  connect.sendAnswer(target, answer).catch((err: Error) => { /* ... */ })
+})
+
+peer.addEventListener('icecandidate', ({ candidate }: ICECandidateEvent) => {
+  if (candidate) {
+    // send the ICE candidate to the remote peer through the signaling server
+    connect.sendICECandidate(target, candidate).catch((err: Error) => { /* ... */ })
+  }
+})
+
+// Listen to incoming offers
+connect.addEventListener('offer', ({ detail: { origin, offer } }: IncomingOfferEvent) => {
+  if (origin === target) {
+    peer.handleIncomingOffer(offer)
+  }
+})
+
+// Listen to incomging answers
+connect.addEventListener('answer', ({ detail: { origin, answer } }: IncomingAnswerEvent) => {
+  if (origin === target) {
+    peer.handleIncomingAnswer(offer)
+  }
+})
+
+// Listen to incoming ICE candidates
+connect.addEventListener('ice', ({ detail: { origin, candidate } }: IncomingICECandidateEvent) => {
+  if (origin === target) {
+    peer.handleIncomingICECandidate(candidate)
+  }
+})
+```
+
+## License
+
+Copyright 2021 Michiel van der Velde.
+
+This software is licensed under [the MIT License](LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,253 @@
+import { nanoid } from 'nanoid'
+
+export type IncomingOfferEvent = CustomEvent<{ origin: string, offer: RTCSessionDescription }>
+export type IncomingAnswerEvent = CustomEvent<{ origin: string, answer: RTCSessionDescription }>
+export type IncomingICECandidateEvent = CustomEvent<{ origin: string, candidate: RTCIceCandidate }>
+
+export interface Request {
+  id?: string,
+  cmd: string,
+  target: string,
+  data: {
+    offer?: RTCSessionDescription,
+    answer?: RTCSessionDescription,
+    candidate?: RTCIceCandidate
+  }
+}
+
+export interface Response {
+  id: string,
+  ok: boolean,
+  data?: {
+    message: string
+  }
+}
+
+export type ConnectInit = {
+  reconnectOnClose?: boolean
+  reconnectOnError?: boolean
+  reconnectInterval?: number,
+  reconnectAttempts?: number,
+  urlTransform?: (previousUrl: string) => string | Promise<string>
+}
+
+const defaultInit: Required<ConnectInit> = {
+  reconnectOnClose: false,
+  reconnectOnError: true,
+  reconnectInterval: 2000,
+  reconnectAttempts: 2,
+  urlTransform: previousUrl => previousUrl
+}
+
+export const PROTOCOL = 'Signal-Fire@3'
+
+export default class Connect extends EventTarget {
+  public readonly id?: string
+  private socket: WebSocket | null = null
+  private hadError = false
+  private readonly pendingRequests: Map<string, (response: Response) => void> = new Map()
+  private readonly config: Required<ConnectInit>
+  private previousUrl?: string
+  private reconnectAttempts = 0
+
+  public constructor (init: ConnectInit = {}) {
+    super()
+    this.config = { ...defaultInit, ...init }
+  }
+
+  public async connect (url: string): Promise<void> {
+    if (this.socket) {
+      throw new Error('socket already created')
+    }
+
+    return new Promise<void>((resolve, reject) => {
+      const removeListeners = () => {
+        socket.removeEventListener('message', handleMessage)
+        socket.removeEventListener('error', handleError)
+        socket.removeEventListener('close', handleClose)
+      }
+
+      const handleMessage = ({ data }: MessageEvent<any>) => {
+        removeListeners()
+
+        if (typeof data !== 'string') {
+          reject(new Error('expected a string message'))
+          return
+        }
+
+        let message: any
+
+        try {
+          message = JSON.parse(data)
+        } catch (e) {
+          reject(new Error('unable to parse message'))
+          return
+        }
+
+        if (message.cmd !== 'welcome') {
+          reject(new Error('expected welcome message'))
+          return
+        }
+
+        if (!message.data.id) {
+          reject(new Error('expected welcome message to have our id'))
+          return
+        }
+
+        this.handleConnected(message.data.id, socket)
+        resolve()
+      }
+
+      const handleError = (ev: any) => {
+        removeListeners()
+        reject(ev.error ?? ev)
+      }
+
+      const handleClose = (ev: CloseEvent) => {
+        removeListeners()
+        reject(new Error(`socket closed unexpectedly with code ${ev.code} (${ev.reason ?? 'no reason'})`))
+      }
+
+      const socket = new WebSocket(url, PROTOCOL)
+
+      socket.addEventListener('message', handleMessage)
+      socket.addEventListener('error', handleError)
+      socket.addEventListener('close', handleClose)
+    })
+  }
+
+  public async close (code?: number, reason?: string): Promise<void> {
+    return new Promise<void>(resolve => {
+      this.addEventListener('close', () => resolve(), { once: true })
+      this.socket?.close(code, reason)
+    })
+  }
+
+  public async sendOffer (target: string, offer: RTCSessionDescription): Promise<void> {
+    const response = await this.request({
+      cmd: 'offer',
+      target,
+      data: { offer }
+    })
+
+    if (!response.ok) {
+      throw new Error(response.data?.message)
+    }
+  }
+
+  public async sendAnswer (target: string, answer: RTCSessionDescription): Promise<void> {
+    const response = await this.request({
+      cmd: 'offer',
+      target,
+      data: { answer }
+    })
+
+    if (!response.ok) {
+      throw new Error(response.data?.message)
+    }
+  }
+
+  public async sendICECandidate (target: string, candidate: RTCIceCandidate): Promise<void> {
+    const response = await this.request({
+      cmd: 'ice',
+      target,
+      data: { candidate }
+    })
+
+    if (!response.ok) {
+      throw new Error(response.data?.message)
+    }
+  }
+
+  private async request (message: Request): Promise<Response> {
+    const id = message.id = nanoid()
+    return new Promise<Response>(resolve => {
+      this.pendingRequests.set(id, resolve)
+      this.socket?.send(JSON.stringify(message))
+    })
+  }
+
+  private handleConnected (id: string, socket: WebSocket): void {
+    // @ts-expect-error
+    this.id = id
+    this.socket = socket
+
+    socket.addEventListener('message', this.handleMessageEvent)
+    socket.addEventListener('error', this.handleErrorEvent)
+    socket.addEventListener('close', this.handleCloseEvent)
+  }
+
+  private handleMessageEvent ({ data }: MessageEvent<any>): void {
+    // TODO: Add data validation
+    const message = JSON.parse(data)
+
+    switch (message.cmd) {
+      case 'offer':
+        this.dispatchEvent(new CustomEvent<{ origin: string, offer: RTCSessionDescription }>('offer', {
+          detail: { origin: message.origin, offer: message.data.offer }
+        }))
+        break
+      case 'answer':
+        this.dispatchEvent(new CustomEvent<{ origin: string, answer: RTCSessionDescription }>('answer', {
+          detail: { origin: message.origin, answer: message.data.answer }
+        }))
+        break
+      case 'ice':
+        this.dispatchEvent(new CustomEvent<{ origin: string, candidate: RTCSessionDescription }>('ice', {
+          detail: { origin: message.origin, candidate: message.data.candidate }
+        }))
+        break
+    }
+  }
+
+  private handleErrorEvent (): void {
+    this.hadError = true
+  }
+
+  private handleCloseEvent (ev: CloseEvent): void {
+    const socket = this.socket as WebSocket
+
+    socket.removeEventListener('message', this.handleMessageEvent)
+    socket.removeEventListener('error', this.handleErrorEvent)
+    socket.removeEventListener('close', this.handleCloseEvent)
+
+    this.socket = null
+
+    if (this.config.reconnectAttempts > 0 && (this.config.reconnectOnClose || (this.hadError && this.config.reconnectOnError))) {
+      this.hadError = false
+
+      setTimeout(() => {
+        this.handleReconnect()
+      }, this.config.reconnectInterval)
+    }
+  }
+
+  private async handleReconnect (): Promise<void> {
+    this.reconnectAttempts++
+
+    if (this.reconnectAttempts > this.config.reconnectAttempts) {
+      this.dispatchEvent(new CustomEvent<Error>('error', {
+        detail: new Error('exceeded maximum reconnect attempts')
+      }))
+      return
+    }
+
+    try {
+      const urlTransform = this.config.urlTransform(<string>this.previousUrl)
+      const url = urlTransform instanceof Promise ? await urlTransform : urlTransform
+
+      try {
+        await this.connect(url)
+        this.reconnectAttempts = 0
+      } catch (e) {
+        setTimeout(() => {
+          this.handleReconnect()
+        }, this.config.reconnectInterval)
+      }
+    } catch (e) {
+      this.dispatchEvent(new CustomEvent<Error>('error', {
+        detail: new Error(`unable to reconnect: ${e.message}`)
+      }))
+    }
+  }
+}
